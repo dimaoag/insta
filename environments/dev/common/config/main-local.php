30bb92464cbb732a776b2c2be407292eff1b8c90
Dmytro Orikhovskyi <dimaoag@gmail.com>
2018-02-20 13:26:20 +0200

    modules user

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -4,7 +4,7 @@ return [
         'db' => [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
+            'email' => 'root',
             'password' => '',
             'charset' => 'utf8',
         ],
